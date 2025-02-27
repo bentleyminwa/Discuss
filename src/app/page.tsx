2cbbd32cf577cd964de09234f358234c8f899d21
Bentley Minwa <bentleyminwa@gmail.com>
2025-02-27 17:29:49 +0300

    Implemented user authentication with signIn and signOut forms; add session management and user profile display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
+import { signIn, signOut } from "@/actions";
+import { auth } from "@/auth";
+import Profile from "@/components/profile";
 import { Button } from "@heroui/react";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
   return (
     <div>
       <h1>Home Page</h1>
-      <Button>Click me</Button>
+      <form action={signIn}>
+        <Button type="submit">Sign In</Button>
+      </form>
+
+      <form action={signOut}>
+        <Button type="submit">Sign Out</Button>
+      </form>
+
+      {session?.user ? (
+        <div>{JSON.stringify(session.user)}</div>
+      ) : (
+        <div>Signed Out!</div>
+      )}
+
+      <Profile />
     </div>
   );
 }
