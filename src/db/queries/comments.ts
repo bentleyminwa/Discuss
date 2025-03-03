08722b53610d4f33b9999974215a40313d15a6dd
Bentley Minwa <bentleyminwa@gmail.com>
2025-03-03 17:49:20 +0300

    Added loading state for post display and optimize comment fetching

--- src/db/queries/comments.ts
+++ src/db/queries/comments.ts
@@ -1,23 +1,24 @@
 import { db } from "@/db";
 import type { Comment } from "@prisma/client";
+import { cache } from "react";
 
 export type CommentWithAuthor = Comment & {
   user: { name: string | null; image: string | null };
 };
 
-export function fetchCommentsById(
-  postId: string
-): Promise<CommentWithAuthor[]> {
-  console.log("Making a query!!!");
-  return db.comment.findMany({
-    where: { postId: postId },
-    include: {
-      user: {
-        select: {
-          name: true,
-          image: true,
+export const fetchCommentsById = cache(
+  (postId: string): Promise<CommentWithAuthor[]> => {
+    console.log("Making a query!!!");
+    return db.comment.findMany({
+      where: { postId: postId },
+      include: {
+        user: {
+          select: {
+            name: true,
+            image: true,
+          },
         },
       },
-    },
-  });
-}
+    });
+  }
+);
